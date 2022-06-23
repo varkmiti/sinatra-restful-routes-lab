2e486a090d54665ebdde196230d314d96212a3a7
Mark Viti <+>
2022-06-23 08:35:14 -0400

    commit tree 1866d2437cc315da2499af3e01571541553de0bc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,43 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get "/recipes/new" do 
+    erb :new
+  end
+
+  post "/recipes" do
+    @recipe = Recipe.create(params)
+
+    redirect 'recipes/' + @recipe.id.to_s
+  end
+
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch "/recipes/:id" do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete "/recipes/:id" do
+    Recipe.destroy(params[:id].to_i)
+    redirect '/recipes'
+  end
+
 end
